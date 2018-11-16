2e7e22efb9e89412c31b509cbb08e0590fb8626e
Rahul Morade <rahulmorade@Rahuls-Mac-mini.local>
2018-11-16 18:31:39 +0530

    Intial
    Intial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppBar from './AppBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+      <div>
+      <AppBar
+      title="AppTitle"
+      />
+      </div>
+      <p>
+      
+      </p>
+      
+      </header>
+      </div>
+      );
+    }
+  }
+  
+  export default App;
+  
